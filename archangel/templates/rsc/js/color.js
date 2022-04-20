7efa36d9b7a6de6425691e7cd88317f2ce466d9b
Slayer Nominee <64646271+slayernominee@users.noreply.github.com>
2022-04-20 19:54:40 +0200

    0.9 templates
    this is made in 2 steps beacause 100 files upload limit

---
+++ archangel/templates/rsc/js/color.js
@@ -0,0 +1,15 @@
+function random_html_color() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function random_rgb_color() {
+    r = Math.floor(Math.random() * 256);
+    g = Math.floor(Math.random() * 256);
+    b = Math.floor(Math.random() * 256);
+    color = 'rgb(' + String(r) + ',' + String(g) + ',' + String(b) + ')';
+    return color;
+}
